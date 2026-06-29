fd445d2457cdf62bae9dcf67b01bde6659bef6e7
LinkNexus <deitykings@gmail.com>
2026-06-29 11:21:26 +0200

    Updated config

--- lua/plugins/lsp/servers.lua
+++ lua/plugins/lsp/servers.lua
@@ -141,7 +141,7 @@ function M.setup(capabilities)
     },
   }
 
-  require("core.pwsh").setup({
+  require("core.powershell").setup({
     bundle_path = vim.fn.stdpath("data") .. "/mason/packages/powershell-editor-services",
     shell = "pwsh",
     capabilities = capabilities,
