2c8db3d9089a685040f9de21a5a13bd917ee570d
LinkNexus <deitykings@gmail.com>
2026-07-18 20:59:54 +0200

    Added dropbar.nvim

--- lua/plugins/dev.lua
+++ lua/plugins/dev.lua
@@ -95,15 +95,6 @@ return {
   },
   {
     "folke/sidekick.nvim",
-    opts = {
-      -- add any options here
-      cli = {
-        mux = {
-          backend = "zellij",
-          enabled = true,
-        },
-      },
-    },
     keys = {
       {
         "<tab>",
@@ -130,5 +121,19 @@ return {
     keys = {
       { "<leader>gU", "<cmd>Neogit<cr>", desc = "Show Neogit UI" }
     }
+  },
+  {
+    'Bekaboo/dropbar.nvim',
+    -- optional, but required for fuzzy finder support
+    dependencies = {
+      'nvim-telescope/telescope-fzf-native.nvim',
+      build = 'make'
+    },
+    config = function()
+      local dropbar_api = require('dropbar.api')
+      vim.keymap.set('n', '<Leader>;', dropbar_api.pick, { desc = 'Pick symbols in winbar' })
+      vim.keymap.set('n', '[;', dropbar_api.goto_context_start, { desc = 'Go to start of current context' })
+      vim.keymap.set('n', '];', dropbar_api.select_next_context, { desc = 'Select next context' })
+    end
   }
 }
